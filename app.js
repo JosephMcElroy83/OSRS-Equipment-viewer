0ce2e5aa862a23bee7f3afa0e438969b50a551f9
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-08-31 12:46:20 -0400

    working fetch, static currently

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ async function fetchData() {
   try {
     let res = await axios.get("https://api.osrsbox.com/equipment");
     const data = res.data;
-    console.log(data);
-    
+    let equip = res.data._items
+    let endPoint = data._links.last.href.split("=")[1];
+    for (let i = 2; i <= endPoint; i++) {
+      let res = await axios.get(`https://api.osrsbox.com/equipment/?page=${i}`)
+      equip = [...equip, ...res.data._items]
+    }
+    //console.log(equip[0]);
+    let filteredItems = equip.filter(item => {
+      if (item.equipment.slot == "weapon") {
+        return item
+      }
+    })
+    console.log(filteredItems)
+    //call function here to do something with data, passing in equip array
   } catch (error) {
     console.log(error)
   }
 }
 
-//fetchData();
+fetchData();
 
 async function fetchSpecific(category, slot, style, value) {
   try {
@@ -29,3 +41,7 @@ async function fetchSpecific(category, slot, style, value) {
 }
 
 //fetchSpecific('weapons', '2h', 'ranged_strength', '20');
+
+//.filter 
+// if category slot = 
+
